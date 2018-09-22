20365a25b99ab87a2e99aa20ec3f02e6deab2b5e
Kedar Kulkarni <k.kulkarni@interproductec.com>
2018-09-23 01:05:33 +0530

    created blank ember project

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'news-app',
+    podModulePrefix: 'news-app/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
